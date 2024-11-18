125439cfb321cbb86808680006ea81263fd3fe39
Amayyas <amayyas.aouadene@epitech.eu>
2024-11-18 21:35:55 +0100

    [ADD] include folder w/ .header

---
+++ include/my.h
@@ -0,0 +1,62 @@
+/*
+** EPITECH PROJECT, 2024
+** Standard Cworkshop Library
+** File description:
+** header
+*/
+
+#ifndef INCLUDED_MY_H
+    #define INCLUDED_MY_H
+
+    #define M_PI 3.14159265358979323846
+
+    #define BUFFER_SIZE 26
+
+
+    #include <stdlib.h>
+    #include <math.h>
+    #include <time.h>
+    #include <assert.h>
+    #include <stdio.h>
+    #include <wchar.h>
+    #include <nl_types.h>
+    #include <ctype.h>
+    #include <wctype.h>
+    #include <locale.h>
+    #include <setjmp.h>
+    #include <string.h>
+    #include <langinfo.h>
+    #include <signal.h>
+    #include <regex.h>
+    #include <sys/syscall.h>
+    #include <unistd.h>
+
+
+int my_abs(int nb);
+double my_acos(double x);
+char *my_asctime_r(const struct tm *tm, char *buf, size_t buf_size);
+char *my_asctime(const struct tm *time);
+double my_asin(double x);
+void my_assert(int expression);
+double my_atan(double x);
+double my_atan2(double y, double x);
+double my_atof(const char *string);
+int my_atoi(const char *string);
+long my_atol(const char *string);
+void *my_bsearch(const void *key, const void *base,
+    int (*compare) (const void *element1, const void *element2));
+void *my_calloc(size_t num, size_t size);
+double my_ceil(double x);
+double my_cos(double x);
+double my_cosh(double x);
+double my_exp(double x);
+double my_fabs(double x);
+int my_fclose(FILE *stream);
+int my_fgetc(FILE *stream);
+char *my_fgets(char *string, int n, FILE *stream);
+size_t my_fread(void *ptr, size_t size, size_t count, FILE *stream);
+int my_getc(FILE *stream);
+ssize_t my_read(int fd, void *buf, size_t count);
+double my_sqrt(double x);
+
+#endif
